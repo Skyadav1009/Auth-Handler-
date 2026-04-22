26b66695fb52f3d070e41c695bd3a03d6f4e26a3
Shivam Kumar Yadav <jeemainsdtu@gmail.com>
2026-04-22 20:47:47 +0530

    Add UserProfile for roles and update logic

--- Auth/accounts/serializers.py
+++ Auth/accounts/serializers.py
@@ -1,16 +1,24 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import UserProfile
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(
         write_only=True, required=True, style={
             'input_type': 'password'})
+    role = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'email', 'password', 'role')
         extra_kwargs = {'email': {'required': True}}
+
+    def validate_role(self, value):
+        if value.lower() == 'admin':
+            raise serializers.ValidationError("Admin registration is not allowed. Only one admin exists.")
+        if value.lower() not in ['staff', 'employee']:
+            raise serializers.ValidationError("Role does not exist. Please choose either 'staff' or 'employee'.")
+        return value.lower()
 
     def validate_email(self, value):
         if not value.endswith('@gmail.com'):
@@ -22,10 +30,14 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
+        role_value = validated_data.pop('role')
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'])
+        
+        # Create user profile with the selected role
+        UserProfile.objects.create(user=user, role=role_value)
         return user
 
 
--- Auth/manage.py
+++ Auth/manage.py
@@ -1,8 +1,7 @@
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 def main():
-    """Run administrative tasks."""
+    
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'auth_project.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -15,3 +14,5 @@ def main():
     execute_from_command_line(sys.argv)
 if __name__ == '__main__':
     main()
+
+
